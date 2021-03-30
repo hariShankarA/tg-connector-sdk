7a2d8e96221013465d4bd788dd0ae040e49aa052
hari <h.shankar@almoayedtechnologies.com>
2021-03-30 17:11:46 +0300

    adding constructor

--- src/main/java/net/tarabutgateway/bobf/connector/sdk/api/model/common/ConsentedAccount.java
+++ src/main/java/net/tarabutgateway/bobf/connector/sdk/api/model/common/ConsentedAccount.java
@@ -1,9 +1,15 @@
 package net.tarabutgateway.bobf.connector.sdk.api.model.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 
 public class ConsentedAccount {
