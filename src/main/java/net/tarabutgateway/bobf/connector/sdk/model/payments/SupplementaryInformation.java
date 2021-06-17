de291c68e94ade1c3b1ee40b987a7143e01f2bfa
hari <h.shankar@almoayedtechnologies.com>
2021-06-17 14:25:11 +0300

    added changes for supp info in payments request

--- src/main/java/net/tarabutgateway/bobf/connector/sdk/model/payments/SupplementaryInformation.java
+++ src/main/java/net/tarabutgateway/bobf/connector/sdk/model/payments/SupplementaryInformation.java
@@ -2,9 +2,13 @@ package net.tarabutgateway.bobf.connector.sdk.model.payments;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Builder;
+import lombok.Data;
 import net.tarabutgateway.bobf.connector.sdk.model.common.CreditorDebtorAccount;
 import net.tarabutgateway.bobf.connector.sdk.model.paymentcharges.PaymentChargesResponse;
 
+@Data
+@Builder
 public class SupplementaryInformation {
 
 	@JsonProperty("debtorAccount")
