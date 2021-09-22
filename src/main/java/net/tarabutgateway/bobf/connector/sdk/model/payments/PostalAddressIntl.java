f3b2f3c4af68c967d63d0339de3972fb5458d8b9
hari <h.shankar@almoayedtechnologies.com>
2021-09-22 09:14:07 +0300

    adding lombok support

--- src/main/java/net/tarabutgateway/bobf/connector/sdk/model/payments/PostalAddressIntl.java
+++ src/main/java/net/tarabutgateway/bobf/connector/sdk/model/payments/PostalAddressIntl.java
@@ -2,6 +2,11 @@ package net.tarabutgateway.bobf.connector.sdk.model.payments;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PostalAddressIntl {
 
 	@JsonProperty("StreetName")
